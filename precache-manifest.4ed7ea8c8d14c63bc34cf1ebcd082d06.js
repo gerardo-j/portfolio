cdd747419f2c4af17951a920b42a7aaa67027a35
Gerardo <gerardojuarez695@gmail.com>
2023-02-11 11:02:41 -0500

    Updates

---
+++ precache-manifest.4ed7ea8c8d14c63bc34cf1ebcd082d06.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e72296d78b58de0a62ba",
+    "url": "/static/css/main.a999f1bf.chunk.css"
+  },
+  {
+    "revision": "e72296d78b58de0a62ba",
+    "url": "/static/js/main.e72296d7.chunk.js"
+  },
+  {
+    "revision": "12ebbd0fdeab1bd66841",
+    "url": "/static/js/1.12ebbd0f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d88ab86eb7921e3b74bfb87cf8048049",
+    "url": "/static/media/Resume.d88ab86e.jpg"
+  },
+  {
+    "revision": "470f79e0c83c8dc53f502a461f0d1140",
+    "url": "/static/media/Resume.470f79e0.pdf"
+  },
+  {
+    "revision": "2fa9d0ab11ed529051dda07fb0d3b50e",
+    "url": "/static/media/Project-1-Preview.2fa9d0ab.png"
+  },
+  {
+    "revision": "6cdbd3e861d715ae6ae329b8d13b6a32",
+    "url": "/static/media/Project-2-Preview.6cdbd3e8.png"
+  },
+  {
+    "revision": "b524f87b6afd0083795614a6e456b135",
+    "url": "/static/media/Project-3-Preview.b524f87b.png"
+  },
+  {
+    "revision": "430139f2ff5568d1de4ef6fbf336f831",
+    "url": "/static/media/blue.430139f2.mp3"
+  },
+  {
+    "revision": "2bdcb4d18eeef439c86146ff04bc9526",
+    "url": "/static/media/green.2bdcb4d1.mp3"
+  },
+  {
+    "revision": "1ae4d3df9cd233be65a58aefe8f1e688",
+    "url": "/static/media/red.1ae4d3df.mp3"
+  },
+  {
+    "revision": "753e071059bbed360d08dc2181a0ecca",
+    "url": "/static/media/wrong.753e0710.mp3"
+  },
+  {
+    "revision": "a5b0d53de2b5dd3a57b9010864ac3a41",
+    "url": "/static/media/yellow.a5b0d53d.mp3"
+  },
+  {
+    "revision": "fe91b780150e2ad7aff9dd595a7a14ed",
+    "url": "/index.html"
+  }
+];
