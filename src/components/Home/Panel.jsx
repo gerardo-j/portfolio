54e7afb990af8bb70fe29bb5c38cbc56f56d097c
Gerardo <gerardojuarez695@gmail.com>
2023-08-07 21:20:24 -0400

    Fix issues Updated packages to latest versions and resolved issues introduced by the updates. Updated routes, imports, and rendering according to new React Router 6 and React 18 requirements.

--- src/components/Home/Panel.jsx
+++ src/components/Home/Panel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom/Link";
+import {Link} from "react-router-dom";
 import ProjectLink from "./ProjectLink";
 import Paper from "@material-ui/core/Paper";
 import makeStyles from "@material-ui/core/styles/makeStyles";
